ad7f2e1acbc5f0a1750e00a8f77baf6bdacd833c
User <stparchkg@gmail.com>
2023-12-10 22:15:19 +0600

    add PriorityEntity

--- todolist/src/main/java/kg/java/todolist/core/models/entities/PriorityEntity.java
+++ todolist/src/main/java/kg/java/todolist/core/models/entities/PriorityEntity.java
@@ -1,28 +1,30 @@
 package kg.java.todolist.core.models.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import java.util.Objects;
+
 @Entity
-@Table(name = "category",schema = "test6",catalog = "postgres")
+@Table(name = "priority", schema = "test6", catalog = "postgres")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Cacheable
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 @Builder(toBuilder = true)
-public class CategoryEntity {
+public class PriorityEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    @ManyToOne(fetch =FetchType.LAZY)
+    private String color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",referencedColumnName = "id")
     private UserEntity user;
+
 }
