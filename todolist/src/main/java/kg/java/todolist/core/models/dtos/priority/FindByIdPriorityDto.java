101b120d14d86dc5d25eddefa7d82ea0c237c162
User <stparchkg@gmail.com>
2023-12-10 22:17:07 +0600

    add dtos for PriorityEntity

---
+++ todolist/src/main/java/kg/java/todolist/core/models/dtos/priority/FindByIdPriorityDto.java
@@ -0,0 +1,10 @@
+package kg.java.todolist.core.models.dtos.priority;
+
+import kg.java.todolist.core.models.dtos.base.BaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FindByIdPriorityDto extends BaseDto {
+}
