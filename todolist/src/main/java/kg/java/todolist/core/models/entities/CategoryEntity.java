0e26755da2d67ec5707d9a7e666f474919d90c8c
User <stparchkg@gmail.com>
2023-12-10 22:02:53 +0600

    add CategoryEntity

---
+++ todolist/src/main/java/kg/java/todolist/core/models/entities/CategoryEntity.java
@@ -0,0 +1,27 @@
+package kg.java.todolist.core.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Builder(toBuilder = true)
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch =FetchType.LAZY)
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    private UserEntity user;
+}
